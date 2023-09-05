4b3a29fc52d1f4717bbb9a7c878208123499850c
RbrtNkvi <robert_nokkvi@hotmail.com>
2023-09-05 17:44:06 +0000

    initial commit

---
+++ src/main/java/is/hi/hbv501g/netkaffi/NetkaffiApplication.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501g.netkaffi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetkaffiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetkaffiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/is/hi/hbv501g/netkaffi/NetkaffiApplicationTests.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501g.netkaffi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetkaffiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
