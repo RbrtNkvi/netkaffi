85a9604492ccbf66181829fbd096d55292134b98
VikingurB <112984463+VikingurB@users.noreply.github.com>
2023-09-08 16:28:43 +0000

    first commit

--- src/main/java/is/hi/hbv501g/netkaffi/NetkaffiApplication.java
+++ src/main/java/is/hi/hbv501g/netkaffi/NetkaffiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NetkaffiApplication {
-
+//ég er að hjálpa, vonandi...
     public static void main(String[] args) {
         SpringApplication.run(NetkaffiApplication.class, args);
     }
