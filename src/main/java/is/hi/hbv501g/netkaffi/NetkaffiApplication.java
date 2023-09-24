3dd6c8ac05324c121a9011f3730109c4e2888fed
RbrtNkvi <robert_nokkvi@hotmail.com>
2023-09-24 23:17:46 +0000

    ""

--- src/main/java/is/hi/hbv501g/netkaffi/NetkaffiApplication.java
+++ src/main/java/is/hi/hbv501g/netkaffi/NetkaffiApplication.java
@@ -1,9 +1,12 @@
 package is.hi.hbv501g.netkaffi;
 
+import jakarta.persistence.Entity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.annotation.Id;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class NetkaffiApplication {
 
     public static void main(String[] args) {
@@ -11,3 +14,4 @@ public class NetkaffiApplication {
     }
 
 }
+
